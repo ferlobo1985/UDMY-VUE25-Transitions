2172d67045f670fb0909b13e5344cabd6a062ee7
Coding rev <fernando.lobo.prez@gmail.com>
2025-08-11 04:07:28 -0300

    01-Basic transition
    01-Basic transition

--- src/components/transitions/one.vue
+++ src/components/transitions/one.vue
@@ -1,13 +1,29 @@
 <template>
-    <div class="p-3 mb-2 bg-success text-white"
-        v-if="data.display"
-    >
-        Hello
-    </div>
+    <transition>
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.display"
+        >
+            Hello
+        </div>
+    </transition>
     <button class="btn btn-primary" 
         @click="data.display = !data.display"
     >
         Toggle
+    </button>
+    <hr/>
+
+     <transition>
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.display2"
+        >
+            Hello
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.display2 = !data.display2"
+    >
+        Toggle display 2
     </button>
 </template>
 
@@ -15,6 +31,24 @@
    import { reactive } from 'vue';
 
    const data = reactive({
-    display:false
+    display:false,
+    display2:true
    })
 </script>
+
+<style scoped>
+
+    .v-enter-from,
+    .v-leave-to {
+        opacity: 0;
+    }
+    .v-enter-active,
+    .v-leave-active {
+        transition: 1s;
+    }
+    .v-enter-to,
+    .v-leave-from {
+        opacity: 1;
+    }
+    
+</style>
