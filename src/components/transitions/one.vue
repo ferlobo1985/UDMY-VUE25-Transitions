a12143d272f5ead87e7eab7b570017e4a7caebce
Coding rev <fernando.lobo.prez@gmail.com>
2025-08-11 04:21:43 -0300

    02-Named transitions
    02-Named transitions

--- src/components/transitions/one.vue
+++ src/components/transitions/one.vue
@@ -25,6 +25,39 @@
     >
         Toggle display 2
     </button>
+
+    <hr/>
+
+
+    <transition name="slide">
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.display3"
+        >
+            Hello
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.display3 = !data.display3"
+    >
+        Toggle display 3
+    </button>
+
+    <hr/>
+
+
+     <transition name="slideup">
+        <div class="p-3 mb-2 bg-success text-white"
+            v-if="data.display4"
+        >
+            Hello
+        </div>
+    </transition>
+    <button class="btn btn-primary" 
+        @click="data.display4 = !data.display4"
+    >
+        Toggle display 4
+    </button>
+
 </template>
 
 <script setup>
@@ -32,7 +65,9 @@
 
    const data = reactive({
     display:false,
-    display2:true
+    display2:true,
+    display3:true,
+    display4:false
    })
 </script>
 
@@ -50,5 +85,65 @@
     .v-leave-from {
         opacity: 1;
     }
+
+    /* SLIDE */
+
+    .slide-enter-from {
+        transform: translateX(20px);
+        opacity: 0;
+    }
+    .slide-enter-active {
+        transition:all 1s ease;
+    }
+    .slide-enter-to {
+        opacity: 1;
+    }
+
+    .slide-leave-from {
+        opacity: 1;
+        background-color: red !important;
+    }
+    .slide-leave-active {
+        transition:all .3s ease;
+    }
+    .slide-leave-to {
+        background-color: black !important;
+        transform: translateX(100px);
+        opacity: 0;
+    }
+
+
+    .slideup-enter-active {
+        animation: slideUp .5s ease-in-out;
+    }
+
+    .slideup-leave-active {
+        animation: slideDown .5s ease-in-out;
+    }
+
+
+
+    @keyframes slideUp {
+        from {
+            opacity: 0;
+            transform: translateY(10px)
+        } to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+     @keyframes slideDown {
+        from {
+            opacity: 1;
+            transform: translateY(0);
+        } to {
+            opacity: 0;
+            transform: translateY(10px)
+        }
+    }
+
     
+    
+
 </style>
